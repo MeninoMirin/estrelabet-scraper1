517eec20779164ef6dca38e2539c1498c14abba3
MeninoMirin <tiagodearvalho19041984@gmail.com>
2024-08-27 21:29:53 -0300

    Update scrape.py

--- api/scrape.py
+++ api/scrape.py
@@ -1,12 +1,28 @@
-from flask import Flask, jsonify
-from scraper import scrape_estrelabet
+from requests_html import HTMLSession
 
-app = Flask(__name__)
+def verificar_estrategia(lista):
+    for numero in lista[:4]:
+        if numero >= 2:
+            return False
+    return True
 
-@app.route('/api/scrape', methods=['GET'])
-def scrape():
-    result = scrape_estrelabet()
-    return jsonify(result=result)
+def scrape_estrelabet():
+    session = HTMLSession()
+    response = session.get('https://estrelabet.com/ptb/bet/main')
+    response.html.render(sleep=5)  # Renderiza o JS
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    accept_cookies = response.html.find('#cookies-bottom-modal a', first=True)
+    if accept_cookies:
+        accept_cookies.click()
+        response.html.render(sleep=2)
+
+    response = session.get('https://estrelabet.com/ptb/games/detail/casino/normal/7787')
+    response.html.render(sleep=5)
+
+    result_history = response.html.find('.result-history', first=True)
+    if result_history:
+        resultados = [float(n.replace('x', '')) for n in result_history.text.split('\n')[:10]]
+        if verificar_estrategia(resultados):
+            return f'Estratégia ok -> {resultados[:4]}'
+        return resultados
+    return 'Nenhum resultado encontrado'
