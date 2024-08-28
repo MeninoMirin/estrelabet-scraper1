fa304605b06b53ad0679e78e6f66317c09ca63a6
MeninoMirin <tiagodearvalho19041984@gmail.com>
2024-08-27 21:52:21 -0300

    Update scrape.py

--- api/scrape.py
+++ api/scrape.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome import service
-from webdriver_manager.opera import OperaDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 def verificar_estrategia(lista):
@@ -18,9 +18,7 @@ LOGIN = 'mario@tmpeml.com'
 PASSWORD = 'Aviator102030!'
 
 def handler(request):
-    webdriver_service = service.Service(OperaDriverManager().install())
-    webdriver_service.start()
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--disable-logging')
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
@@ -32,63 +30,16 @@ def handler(request):
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_experimental_option('w3c', True)
 
-    driver = webdriver.Remote(webdriver_service.service_url, options=options)
-    driver.maximize_window()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    driver.get('https://estrelabet.com/ptb/bet/main')
-
-    sleep(10)
-    print('Atualizando site...')
-    driver.refresh()
-
-    print('Aguardando botão para aceitar cookies...')
-
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="cookies-bottom-modal"]/div/div[1]/a'))
-    )
-
-    sleep(10)
-    driver.find_element(By.XPATH, '//*[@id="cookies-bottom-modal"]/div/div[1]/a').click()
-    sleep(3)
-
-    print('Botão clicado')
-    print('iniciando login...')
-    driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(LOGIN)
-    print('Usuário')
-    sleep(3)
-    driver.find_element(By.XPATH, '//*[@id="password-login"]').send_keys(PASSWORD)
-    print('Senha')
-    sleep(3)
-    driver.find_element(By.XPATH, '//*[@id="header"]/div/div[1]/div/div[2]/app-login/form/div/div/div[2]/button').click()
-    print('botão de acesso clicado')
-    sleep(5)
+    driver.get('https://estrelabet.com/ptb/games/detail/casino/normal/7787')
+    sleep(10)  # Espera para garantir que a página carregue
 
     print('Acessando aviator...')
-    driver.get('https://estrelabet.com/ptb/games/detail/casino/normal/7787')
-    print('Aguardando Frame...')
+    # Adicione mais código aqui para interagir com a página
 
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.ID, 'gm-frm'))
-    )
-    sleep(5)
-
-    iframe = driver.find_element(By.ID, 'gm-frm')
-    driver.switch_to.frame(iframe)
-    sleep(5)
-
-    while True:
-        resultados = [float(n) for n in driver.find_element(
-            By.CLASS_NAME, 'result-history').text.replace(
-            'x', '').split('\n')][:10]
-        if verificar_estrategia(resultados):
-            print(f'estrategia ok -> {resultados[:4]}')
-        print(resultados)
-        while True:
-            verificacao = [float(n) for n in driver.find_element(
-                By.CLASS_NAME, 'result-history').text.replace(
-                'x', '').split('\n')][:10]
-            if verificacao != resultados:
-                break
+    # Feche o navegador após o uso
+    driver.quit()
 
     return {
         'statusCode': 200,
